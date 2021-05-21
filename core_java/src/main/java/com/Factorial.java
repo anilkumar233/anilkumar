5b175246d280f01751d686dba0b447bb5c74c86e
anilk <anilk@LAPTOP-QH82A1D2>
2021-05-21 17:46:37 +0530

    my first commit

--- core_java/src/main/java/com/Factorial.java
+++ core_java/src/main/java/com/Factorial.java
@@ -3,7 +3,6 @@ package com;
 public class Factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		 int i,fact=1;  
 		  int number=5;//It is the number to calculate factorial    
 		  for(i=1;i<=number;i++){    
