87624a93d9c711a302f615a81e7a39c6a5e16ff7
anilk <anilk@LAPTOP-QH82A1D2>
2021-05-25 07:41:26 +0530

    my second commit

--- core_java/src/main/java/com/While_loop.java
+++ core_java/src/main/java/com/While_loop.java
@@ -4,7 +4,7 @@ public class While_loop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int m=0;
+int m=1;
 while(m<=10)
 {
 	System.out.println("kumar");
--- core_java/src/main/java/com/add_two_num.java
+++ core_java/src/main/java/com/add_two_num.java
@@ -6,7 +6,7 @@ public class add_two_num {
 int i=5;
 int j=7;
 int k=i+j;
-System.out.println("Result is"+k);
+System.out.println("Result is "+k);
 	}
 
 }
